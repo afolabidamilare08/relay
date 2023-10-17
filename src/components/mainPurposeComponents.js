10ae0044688b5e5a2e71e07d97b9e4e8adc51f07
Afolabi Damilare <afolabidamilare08@gmail.com>
2023-10-17 07:26:45 +0100

    sksks

--- src/components/mainPurposeComponents.js
+++ src/components/mainPurposeComponents.js
@@ -2,8 +2,33 @@ import UnderlineImg from '../assets/images/underline.png';
 import LockImg from '../assets/images/lock.png';
 import VoteImg from '../assets/images/vote.png';
 import BoostImg from '../assets/images/boost.png';
+import {  motion } from 'framer-motion';
 
 const MainpurposeComponents = () => {
+
+    const Details = [
+        { img: LockImg, title:"Unlocking Liquidity for veTokens", message: `VeTokens, also known as vote-escrowed tokens, 
+        have been synonymous with decentralized governance 
+        but faced liquidity challenges. Relay, an Over-The-Counter 
+        (OTC) P2P trading solution, transforms this narrative by 
+        providing immediate liquidity. Sellers can now unlock 
+        the intrinsic value of their assets without waiting 
+        for lockup periods to expire, offering new financial possibilities.` },
+
+        { img: VoteImg, title:"Enhancing Voting Power", message: `Buyers using Relay can acquire veTokens at below-market prices, 
+        enhancing their voting power within decentralized governance 
+        structures cost-effectively. This approach fosters a more 
+        decentralized and inclusive distribution of voting power, 
+        benefiting the entire ecosystem.` },
+
+        { img: BoostImg, title:"Boosting Confidence and Liquidity", message: `Relay's impact extends beyond individual users. 
+        By enabling OTC trading of veTokens, 
+        it boosts confidence in this asset class, 
+        attracting more participants to veToken governance. 
+        This increase in participation and liquidity creates 
+        a virtuous cycle, reinforcing the value of veTokens 
+        and attracting additional resources to decentralized ecosystems.` },
+    ]
 
     return (
 
@@ -24,57 +49,26 @@ const MainpurposeComponents = () => {
 
                 <div className='our_purpose_home_main' >
 
-                    <div className='our_purpose_home_main_div' >
+                    { Details.map( ( det, index ) => {
+                        return (
 
-                        <img alt='' src={LockImg} />
+                            <motion.div 
+                                className='our_purpose_home_main_div'  
+                                initial={{ scaleX: 0, transformOrigin: '0% 0%' }}
+                                whileInView={{ scaleX: 1, transformOrigin: '0% 0%' }}
+                                transition={{ duration: 0.6 }}
+                                viewport={{ once: true }}
+                            >
+                                <img alt='' src={det.img} />
 
-                        <h5>Unlocking Liquidity for veTokens</h5>
+                                <h5>{det.title}</h5>
 
-                        <h6>
-                            VeTokens, also known as vote-escrowed tokens, 
-                            have been synonymous with decentralized governance 
-                            but faced liquidity challenges. Relay, an Over-The-Counter 
-                            (OTC) P2P trading solution, transforms this narrative by 
-                            providing immediate liquidity. Sellers can now unlock 
-                            the intrinsic value of their assets without waiting 
-                            for lockup periods to expire, offering new financial possibilities.
-                        </h6>
+                                <h6>{det.message}</h6>
 
-                    </div>
+                            </motion.div>
 
-                    <div className='our_purpose_home_main_div' >
-
-                        <img alt='' src={VoteImg} />
-
-                        <h5>Enhancing Voting Power</h5>
-
-                        <h6>
-                            Buyers using Relay can acquire veTokens at below-market prices, 
-                            enhancing their voting power within decentralized governance 
-                            structures cost-effectively. This approach fosters a more 
-                            decentralized and inclusive distribution of voting power, 
-                            benefiting the entire ecosystem.
-                        </h6>
-
-                    </div>
-
-                    <div className='our_purpose_home_main_div' >
-
-                        <img alt='' src={BoostImg} />
-
-                        <h5>Boosting Confidence and Liquidity</h5>
-
-                        <h6>
-                            Relay's impact extends beyond individual users. 
-                            By enabling OTC trading of veTokens, 
-                            it boosts confidence in this asset class, 
-                            attracting more participants to veToken governance. 
-                            This increase in participation and liquidity creates 
-                            a virtuous cycle, reinforcing the value of veTokens 
-                            and attracting additional resources to decentralized ecosystems.
-                        </h6>
-
-                    </div>
+                        )
+                    } ) }
 
                 </div>
 
