9a028e449377969959cd19bd432addb79523f859
Afolabi Damilare <afolabidamilare08@gmail.com>
2023-10-19 00:23:32 +0100

    fmff

--- src/dapp/dapp.otc.js
+++ src/dapp/dapp.otc.js
@@ -2,10 +2,13 @@ import EthImg from '../assets/images/eth.png';
 import BnbImg from '../assets/images/bnb.png';
 import ArmImg from '../assets/images/arm.png';
 import {BsChevronDown, BsFunnel} from 'react-icons/bs';
-import {HiOutlineFunnel} from 'react-icons/hi2'
+import {FaExchangeAlt} from 'react-icons/fa';
+import {FiExternalLink} from 'react-icons/fi';
+import {BiSolidCopy} from 'react-icons/bi';
 import ArrowUp from '../assets/images/Arrow_Up_Icon.png';
 import {Popover, PopoverTrigger, PopoverContent, Switch} from "@nextui-org/react";
 import { Link } from 'react-router-dom';
+import RelayIc from '../assets/images/relay_ic.png';
 
 
 
@@ -97,18 +100,62 @@ const OtcDapp = () => {
 
                 <div className='Otc_main_body_div' >
 
-                    <h5 className='Otc_main_body_div_title' >RLY / ETH</h5>
+                    <div className='Otc_main_body_div_top' >
+                        <h5 className='Otc_main_body_div_top_left' >RLY / ETH</h5>
+                        <div className='Otc_main_body_div_top_right' >
+                            <h6 className='Otc_main_body_div_top_right_txt' >Copy Trade URL</h6>
+                            <BiSolidCopy className='Otc_main_body_div_top_right_ic' />
+                        </div>
+                    </div>
 
                     <div className='Otc_main_body_div_GW' >
-                        <h6>
+                        <h6 className='Otc_main_body_div_GW_txt' >
                             <span>Gives:</span>
                             10,123
                         </h6>
-                        <h6>
+                        <h6 className='Otc_main_body_div_GW_txt' >
                             <span>Wants:</span>
                             5.00
                         </h6>
                     </div>
+
+                    <div className='Otc_main_body_div_mid' >
+
+                        <h5>($10,123)</h5>
+
+                        <FaExchangeAlt className='Otc_main_body_div_mid_ic' />
+
+                        <h5>($7,742.85)</h5>
+
+                    </div>
+
+                    <div className='Otc_main_body_div_lst' >
+
+                        <div className='Otc_main_body_div_lst_pt' >
+
+                            <img src={RelayIc} alt='' />
+
+                            <h5>RLY</h5>
+
+                            <FiExternalLink className='Otc_main_body_div_lst_pt_ic' />
+
+                        </div>
+
+                        <div className='Otc_main_body_div_lst_pt' >
+
+                            <FiExternalLink className='Otc_main_body_div_lst_pt_ic' />
+
+                            <img src={RelayIc} alt='' />
+
+                            <h5>RLY</h5>
+
+                        </div>
+
+                    </div>
+
+                    <button className='Otc_main_body_div_btn' >
+                        Cancel Trade
+                    </button>
 
                 </div>
 
